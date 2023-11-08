4d4f527d4da346a6c240699298ed1c937c3ba01e
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-11-07 23:41:11 -0500

    Implemented getAll news on view model

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,68 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:trainee/model/news_model.dart';
-import 'package:trainee/services/news_service.dart';
 import 'package:trainee/view_model/news_vm.dart';
 import 'package:trainee/widgets/news_card.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<NewsModel> news = [];
-  NewsService newsService = NewsService();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _getNews();
-  }
-
-  void _getNews() async {
-    print("Obteniendo noticias");
-    news = await newsService.getAll();
-    setState(() {});
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final newsController = Provider.of<NewsVM>(context);
+    final newsVM = Provider.of<NewsVM>(context);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: _appBar(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: news.isEmpty
-            ? const Center(child: CircularProgressIndicator())
-            : _newsContent(newsController),
+        child: _newsContent(newsVM),
       ),
     );
   }
 
   ListView _newsContent(NewsVM newsController) {
     return ListView.separated(
-              itemCount: news.length,
-              itemBuilder: ((context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    newsController.navigateToDetail(news[index]);
-                  },
-                  child: NewsCard(
-                    article: news[index],
-                  ),
-                );
-              }),
-              separatorBuilder: (BuildContext context, int index) {
-                return const Divider(
-                  thickness: 1.0,
-                );
-              },
-            );
+      itemCount: newsController.news.length,
+      itemBuilder: ((context, index) {
+        return GestureDetector(
+          onTap: () {
+            newsController.navigateToDetail(newsController.news[index]);
+          },
+          child: NewsCard(
+            article: newsController.news[index],
+          ),
+        );
+      }),
+      separatorBuilder: (BuildContext context, int index) {
+        return const Divider(
+          thickness: 1.0,
+        );
+      },
+    );
   }
 
   AppBar _appBar() {
--- lib/view_model/news_vm.dart
+++ lib/view_model/news_vm.dart
@@ -2,14 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:trainee/helper/navigator.dart';
 import 'package:trainee/model/news_model.dart';
 import 'package:trainee/pages/detail.dart';
+import 'package:trainee/services/news_service.dart';
 
 class NewsVM extends ChangeNotifier {
   final NavigatorService _navigatorService;
+  List<NewsModel> news = [];
 
-  NewsVM(this._navigatorService);
+  NewsVM(this._navigatorService) {
+    getAll();
+  }
 
   navigateToDetail(NewsModel article) {
     //Bad practice to pass context in view model. Thats why we use navigator service
     _navigatorService.goTo(DetailPage(article: article));
+  }
+
+  getAll() async {
+    _navigatorService.showLoader();
+    await Future.delayed(Duration(seconds: 3));
+    news = await NewsService().getAll();
+    _navigatorService.pop();
+    notifyListeners();
   }
 }
